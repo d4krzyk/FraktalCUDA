5ed9c84a657e706de2ee927f868897b35ea597a5
d4krzyk <danielkrzyk44@gmail.com>
2023-09-18 00:26:59 +0200

    final fraktal

--- Fraktal/kernel.cu
+++ Fraktal/kernel.cu
@@ -4,49 +4,17 @@
 #include <fstream>
 #include <cuda_runtime.h>
 //#include <GLFW/glfw3.h>
-#include <GL/glew.h>
-#include <GL/freeglut.h>
-#include <cuda_gl_interop.h>
-
+//#include <GL/glew.h>
+//#include <GL/freeglut.h>
+//#include <cuda_gl_interop.h>
+#include <opencv2/opencv.hpp>
 #define WIDTH 800
 #define HEIGHT 800
 
 
-GLuint texture; // ID tekstury OpenGL
-cudaGraphicsResource* cudaTextureResource; // CUDA resource dla tekstury
-
-
-// Funkcja inicjalizująca OpenGL
-void initOpenGL(int argc, char** argv) {
-    // Inicjalizacja GLUT
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(WIDTH, HEIGHT); // Ustaw rozmiar okna
-    glutInitWindowPosition(0, 0);
-    glClearColor(1.0, 1.0, 1.0, 1.0);
-    /*glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-2, 2, 2, -2, 1, -1);*/
-    // Stwórz okno GLUT
-    glutCreateWindow("CUDA-Mandelbrot");
-
-    // Inicjalizacja GLEW dla obsługi rozszerzeń OpenGL
-    glewInit();
-
-    // Utwórz teksturę OpenGL
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, WIDTH, HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    // Zarejestruj teksturę w CUDA
-    cudaGraphicsGLRegisterImage(&cudaTextureResource, texture, GL_TEXTURE_2D, cudaGraphicsRegisterFlagsWriteDiscard);
-}
-
 
 __global__
-void mandelbrotGPU(float* output, int width, int height, float xmin, float xmax, float ymin, float ymax, int max_iter, float chaos_c) {
+void mandelbrotGPU(float* output, int width, int height, float xmin, float xmax, float ymin, float ymax, int max_iter, float chaos_cr, float chaos_ci) {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
 
@@ -54,7 +22,7 @@ void mandelbrotGPU(float* output, int width, int height, float xmin, float xmax,
         float real = xmin + (xmax - xmin) * x / width;
         float imag = ymin + (ymax - ymin) * y / height;
 
-        float2 c = make_float2(real * chaos_c, imag * chaos_c);
+        float2 c = make_float2(real * chaos_cr, imag * chaos_ci);
         float2 z = c;
 
         int color = max_iter;
@@ -74,7 +42,7 @@ void mandelbrotGPU(float* output, int width, int height, float xmin, float xmax,
 }
 
 __host__
-void mandelbrotCPU(float* output, int width, int height, float xmin, float xmax, float ymin, float ymax, int max_iter, float chaos_c) {
+void mandelbrotCPU(float* output, int width, int height, float xmin, float xmax, float ymin, float ymax, int max_iter, float chaos_cr, float chaos_ci) {
 
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
@@ -83,7 +51,7 @@ void mandelbrotCPU(float* output, int width, int height, float xmin, float xmax,
 
 
 
-            float2 c = make_float2(real * chaos_c, imag * chaos_c);
+            float2 c = make_float2(real * chaos_cr, imag * chaos_ci);
             float2 z = c;
 
             int color = max_iter;
@@ -102,77 +70,32 @@ void mandelbrotCPU(float* output, int width, int height, float xmin, float xmax,
         }
     }
 }
-__host__
-void saveArrayToFile(const char* filename, float* data, int size) {
-    std::ofstream file(filename, std::ios::binary);
-    file.write(reinterpret_cast<char*>(data), size * sizeof(float));
-    file.close();
-}
-__host__
-void saveParameters(float xmin, float xmax, float ymin, float ymax) {
-    std::ofstream file("parameters_XY.txt");
-    if (file.is_open()) {
-        file << xmin << " " << xmax << " " << ymin << " " << ymax;
-        file.close();
-        std::cout << "Parametry zapisane do pliku." << std::endl;
-
-    }
-    else {
-        std::cerr << "Nie można otworzyć pliku do zapisu." << std::endl;
-    }
-
-}
-
-
-
-__global__ void processImage(float* input, uchar4* output, int width, int height) {
-    int x = blockIdx.x * blockDim.x + threadIdx.x;
-    int y = blockIdx.y * blockDim.y + threadIdx.y;
-
-    if (x < width && y < height) {
-        float value = input[y * width + x];
-        uchar4 color = make_uchar4(value * 255, value * 255, value * 255, 255);
-        output[y * width + x] = color;
-    }
-}
-
-void Display() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POINTS);
-
-    
-
-    glEnd();
-    glutSwapBuffers();
-}
-
 
 
 
 
 int main(int argc, char** argv) {
 
-    // Inicjalizacja OpenGL
-    initOpenGL(argc, argv);
+    const int width = WIDTH;
+    const int height = HEIGHT;
+
+    float var_cr = 0.89f;
+    float var_ci = 0.89f;
+    const int max_iter = 120;
 
 
-    const int width = 1600;
-    const int height = 1600;
-    float var_c = 0.89;
+    const float xmin = -2.0f;     const float xmax = 1.0f;
+    const float ymin = -1.5f;     const float ymax = 1.5f;
 
-    //const float xmin = -2.0f;     const float xmax = 1.0f;
-    //const float ymin = -1.5f;     const float ymax = 1.5f;
+    
 
-    const int max_iter = 50;
-
-    const float xmin = -2.0f;       const float xmax = -1.54f;
-    const float ymax = 0.25f;       const float ymin = -0.25f;
+    //const float xmin = -2.0f;       const float xmax = -1.54f;
+    //const float ymax = 0.25f;       const float ymin = -0.25f;
 
 
-    saveParameters(xmin, xmax, ymin, ymax);
 
-    float* outputCPU = new float[width * height];
-    float* outputGPU;
+    float* outputGPU; // Dane w pamięci GPU
+    float* outputCPU = new float[width * height]; // Dane w pamięci CPU
     cudaMallocManaged(&outputGPU, width * height * sizeof(float));
 
 
@@ -183,30 +106,14 @@ int main(int argc, char** argv) {
 
     do
     {
-        //std::cout << "Podaj zmienną c z przedziału 0-1 np. 0.45: ";
-        //std::cin >> var_c;
-    } while (var_c > 1.00 && var_c < 0.0);
+        //std::cout << "Podaj 2 zmienne cr (real) i ci (imaginary) z przedziału 0-1 (zalecane jest podawanie wartości większych od 0.5) np. 0.75: ";
+        //std::cin >> var_cr >> var_ci;
+    } while (var_cr > 1.00 && var_cr < 0.0 && var_ci > 1.00 && var_ci < 0.0);
 
 
 
 
-    // Pomiar czasu dla GPU
-    cudaEvent_t startGPU, stopGPU;
-    cudaEventCreate(&startGPU);
-    cudaEventCreate(&stopGPU);
-
-    cudaEventRecord(startGPU);
-    mandelbrotGPU << <gridSize, blockSize >> > (outputGPU, width, height, xmin, xmax, ymin, ymax, max_iter, var_c);
-    cudaEventRecord(stopGPU);
-    cudaEventSynchronize(stopGPU);
-
-    float elapsedTimeGPU;
-    cudaEventElapsedTime(&elapsedTimeGPU, startGPU, stopGPU);
-    std::cout << "Czas GPU: " << elapsedTimeGPU << " ms" << std::endl;
-
-
-    cudaEventDestroy(startGPU);
-    cudaEventDestroy(stopGPU);
+    
 
     // Pomiar czasu dla CPU
     cudaEvent_t startCPU, stopCPU;
@@ -215,7 +122,7 @@ int main(int argc, char** argv) {
 
     cudaEventRecord(startCPU);
 
-    mandelbrotCPU(outputCPU, width, height, xmin, xmax, ymin, ymax, max_iter, var_c);
+    mandelbrotCPU(outputCPU, width, height, xmin, xmax, ymin, ymax, max_iter, var_cr, var_ci);
     cudaEventRecord(stopCPU);
     cudaEventSynchronize(stopCPU);
 
@@ -223,22 +130,71 @@ int main(int argc, char** argv) {
     cudaEventElapsedTime(&elapsedTimeCPU, startCPU, stopCPU);
     std::cout << "Czas CPU: " << elapsedTimeCPU << " ms" << std::endl;
 
+
     cudaEventDestroy(startCPU);
     cudaEventDestroy(stopCPU);
 
 
+    // Pomiar czasu dla GPU
+    cudaEvent_t startGPU, stopGPU;
+    cudaEventCreate(&startGPU);
+    cudaEventCreate(&stopGPU);
+
+    cudaEventRecord(startGPU);
+    mandelbrotGPU < <<gridSize, blockSize>> > (outputGPU, width, height, xmin, xmax, ymin, ymax, max_iter, var_cr, var_ci);
+    // Przekonwertuj dane na obraz na CPU
+    float* gpuImage = new float[width * height];
+    cudaMemcpy(gpuImage, outputGPU, width * height * sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaEventRecord(stopGPU);
+    cudaEventSynchronize(stopGPU);
+
     cudaDeviceSynchronize();
 
 
-    glutDisplayFunc(Display);
-    glutMainLoop();
+
+    float elapsedTimeGPU;
+    cudaEventElapsedTime(&elapsedTimeGPU, startGPU, stopGPU);
+    std::cout << "Czas GPU: " << elapsedTimeGPU << " ms" << std::endl;
 
 
+    cudaEventDestroy(startGPU);
+    cudaEventDestroy(stopGPU);
+    
 
-    // Zapisz tablicę pixels do pliku
-    saveArrayToFile("output_mandela_arrayGPU.bin", outputGPU, width * height);
-    saveArrayToFile("output_mandela_arrayCPU.bin", outputCPU, width * height);
+    // Twórz obiekt OpenCV Mat z wynikowego obrazu
+    cv::Mat FraktalOutputImageGPU(height, width, CV_32F, gpuImage);
+    cv::Mat FraktalOutputImageCPU(height, width, CV_32F, outputCPU);
+
+    // Pozycja tekstu (lewy górny róg)
+    cv::Point textPosition(10, 30);
+    // Kolor tekstu (BGR)
+    cv::Scalar textColor(255, 255, 255);
+
+    std::string CPUTimeString = "Time CPU: " + std::to_string(elapsedTimeCPU) + "ms";
+    std::string GPUTimeString = "Time GPU: " + std::to_string(elapsedTimeGPU) + "ms";
+
+    // Dodaj tekst do obrazu
+    cv::putText(FraktalOutputImageCPU, CPUTimeString, textPosition, cv::FONT_HERSHEY_SIMPLEX, 1, textColor, 2);
+    cv::putText(FraktalOutputImageGPU, GPUTimeString, textPosition, cv::FONT_HERSHEY_SIMPLEX, 1, textColor, 2);
+
+    cv::Mat colorImageCPU;
+    cv::Mat colorImageGPU;
+
+    FraktalOutputImageCPU.convertTo(colorImageCPU, CV_8UC3, 255);
+    FraktalOutputImageGPU.convertTo(colorImageGPU, CV_8UC3, 255);
+    cv::applyColorMap(colorImageCPU, colorImageCPU, cv::COLORMAP_HOT);
+    cv::applyColorMap(colorImageGPU, colorImageGPU, cv::COLORMAP_HOT);
+
+    // Wyświetl obrazy
+    cv::imshow("CPU Fraktal Mandelbrot", colorImageCPU);
+    cv::imshow("GPU Fraktal Mandelbrot", colorImageGPU);
+
+    cv::waitKey(0);
+
+
     cudaFree(outputGPU);
+    
     delete[] outputCPU;
 
     return 0;
